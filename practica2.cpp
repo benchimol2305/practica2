1b68af578c17c9f6e21064f86abbdb15b352b5c7
benchimol2305 <cesarbenchimoldavila@gamil.com>
2024-10-17 12:00:34 -0400

    1

--- practica2.cpp
+++ practica2.cpp
@@ -1,1 +1,47 @@
-papi2
+#include<iostream>
+#include<string>
+using namespace std;
+struct datospersona
+{
+int id_register;
+char nombre [100];
+int cedula;
+int edad;
+float peso;
+float altura;
+string genero;      
+};
+ int main(){
+const int numero_personasmax= 100;
+datospersona personas[numero_personasmax];
+    int numPersonas = 0;
+    char continuar; 
+    do 
+    {
+     if (numPersonas >= numero_personasmax) {
+            cout << "Se ha alcanzado el máximo número de personas permitidas." << endl;
+            break;
+     cout<<"ingrese el id del registro";
+     cin>>personas[numPersonas].id_register;   
+     cout<<"ingrese el nombre de la persona";
+     cin>>personas[numPersonas].nombre;
+     cout<<"ingrese la cedula de la persona";
+     cin>>personas[numPersonas].cedula;
+     cout<<"ingrese la edad de la persona";
+     cin>>personas[numPersonas].edad;
+     cout<<"ingrese el peso de la persona";
+     cin>>personas[numPersonas].peso;
+     cout<<"ingrese la altura de la persona";
+     cin>>personas[numPersonas].altura;
+     cout<<"ingrese el genero de la persona, 'M' o 'F'";
+     cin>>personas[numPersonas].genero;
+    } while (continuar== 's'|| continuar=='S');
+    
+
+
+
+
+
+
+
+ }
